9ee39b8b8ac77d6b7e3ec8ccdc43ac66de3f601c
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-15 20:47:19 +0100

    getJsonResponse fcuntion is not needed

--- server-node/helpers.js
+++ server-node/helpers.js
@@ -1,19 +1,10 @@
 import fetch from 'node-fetch'
 
-// Get data from Fetch response
-const getJsonResponse = async (response) => {
-  try {
-    return await response.json()
-  } catch (error) {
-    throw new Error(`Something went wrong: Can't parse response!`)
-  }
-}
-
 // Send request to location API
 export const sendHttpRequest = async (endpoint) => {
   try {
     const response = await fetch(endpoint)
-    return await getJsonResponse(response)
+    return response.json()
   } catch (error) {
     throw new Error(`Something went wrong: HTTP Request has failed!`)
   }
