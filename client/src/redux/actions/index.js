b65579d0f6091dadf929a214d6e1a6ef43ff6692
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-17 18:38:35 +0100

    Convert function to async

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,20 +1,31 @@
 export const actionTypes = {
   REQUEST_LOCATION: 'REQUEST_LOCATION',
   REQUEST_LOCATION_SUCCESS: 'REQUEST_LOCATION_SUCCESS',
+  REQUEST_LOCATION_FAILED: 'REQUEST_LOCATION_FAILED',
 }
 
-export const requestLocationAction = () => (dispatch) => {
-  fetch('http://localhost:3000/location')
-    .then((response) => {
-      return response.json()
+const locationApiEndpoint = 'http://localhost:3000/location'
+
+export const requestLocationAction = () => async (dispatch) => {
+  // Send request to get location data (and handle error)
+  const response = await fetch(locationApiEndpoint).catch(() => {
+    dispatch({
+      type: actionTypes.REQUEST_LOCATION_FAILED,
     })
-    .then((data) => {
-      const { location } = data
-      setTimeout(() => {
-        dispatch({
-          type: actionTypes.REQUEST_LOCATION_SUCCESS,
-          payload: location,
-        })
-      }, 1500)
-    })
+  })
+
+  // Get data from response
+  const { location } = await response.json()
+
+  // Add data to redux store
+  dispatch({
+    type: actionTypes.REQUEST_LOCATION_SUCCESS,
+    payload: location,
+  })
 }
+
+/*
+export const requestWeatherAction = (lattitude, longitude, timestamp) => (
+  dispatch
+) => {}
+*/
