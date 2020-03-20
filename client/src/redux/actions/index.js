241f841c9aca26f41a7b2e153c56e67f049f21b7
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-20 20:54:25 +0100

    Add requestWeatherAction action

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,7 +1,9 @@
 export const actionTypes = {
-  REQUEST_LOCATION: 'REQUEST_LOCATION',
   REQUEST_LOCATION_SUCCESS: 'REQUEST_LOCATION_SUCCESS',
   REQUEST_LOCATION_FAILED: 'REQUEST_LOCATION_FAILED',
+  SET_LOCATION: 'SET_LOCATION',
+  REQUEST_WEATHER_SUCCESS: 'REQUEST_WEATHER_SUCCESS',
+  REQUEST_WEATHER_FAILED: 'REQUEST_WEATHER_FAILED',
 }
 
 const locationApiEndpoint = 'http://localhost:3000/location'
@@ -25,8 +27,30 @@ export const requestLocationAction = () => async (dispatch) => {
   })
 }
 
-/*
-export const requestWeatherAction = (lattitude, longitude, timestamp) => (
+export const requestWeatherAction = (lattitude, longitude, timestamp) => async (
   dispatch
-) => {}
-*/
+) => {
+  // Send request to get weather data (and handle error)
+  const response = await fetch(
+    `${weatherApiEndpoint}?time=${Date.now()}`
+  ).catch(() => {
+    dispatch({
+      type: actionTypes.REQUEST_WEATHER_FAILED,
+    })
+  })
+
+  // Get data from response
+  const data = await response.json()
+
+  // Add location data to redux store
+  dispatch({
+    type: actionTypes.SET_LOCATION,
+    payload: data.city,
+  })
+
+  // Add weather data to redux store
+  dispatch({
+    type: actionTypes.REQUEST_WEATHER_SUCCESS,
+    payload: data.daily,
+  })
+}
