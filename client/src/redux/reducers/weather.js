2ca30a18c6eeb0bda0d96fab6c80dd5c95ffc3a9
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-20 20:47:43 +0100

    Create logic to store weather data

---
+++ client/src/redux/reducers/weather.js
@@ -0,0 +1,30 @@
+import { actionTypes } from '../actions'
+
+const getWeatherData = (data) =>
+  data.reduce((acc, item) => {
+    // Get date from timestamp
+    const date = new Date(item.time * 1000)
+    // Create string from date
+    const dateString = date.toISOString().split('T')[0]
+    // Create object with date string key
+    if (typeof acc[dateString] === 'undefined') {
+      acc[dateString] = item
+    }
+    return acc
+  }, {})
+
+const reducer = (state = {}, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case actionTypes.REQUEST_WEATHER_SUCCESS:
+      return {
+        ...state,
+        summary: payload.summary,
+        data: getWeatherData(payload.data),
+      }
+  }
+
+  return state
+}
+
+export default reducer
