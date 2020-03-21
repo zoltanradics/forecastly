3ad7adf56d58b217f55fe55fd1006c82e74508d2
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-21 11:27:39 +0100

    Add new reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import layout from './layout'
 import location from './location'
+import weather from './weather'
 
 export default combineReducers({
   layout,
   location,
+  weather,
 })
