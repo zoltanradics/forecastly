23d2e77ee1cf2673265353bd5a053531ee985b03
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-22 15:16:33 +0100

    Add some animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { CSSTransition } from 'react-transition-group'
 import classNames from 'classnames'
 
 import { requestWeatherAction } from './redux/actions'
@@ -10,10 +11,13 @@ import Display from './components/Display'
 
 const App = () => {
   const dispatch = useDispatch()
+  const [displayMounted, setDisplayMounted] = useState(false)
   const {
     location: { city },
     weather: { daily, currently, weatherLoaded },
   } = useSelector((store) => store)
+
+  const showDispplay = () => setDisplayMounted(true)
 
   useEffect(() => {
     dispatch(requestWeatherAction())
@@ -22,17 +26,22 @@ const App = () => {
   return (
     <main>
       <div className="box">
-        <div
-          className={classNames('box--inner', {
-            'box--inner__align-middle': !weatherLoaded,
-            'box--inner__full-height': weatherLoaded,
-          })}>
-          {weatherLoaded ? (
-            <Display city={city} daily={daily} currently={currently} />
-          ) : (
-            <Loading />
-          )}
-        </div>
+        <CSSTransition
+          in={weatherLoaded}
+          timeout={800}
+          classNames="box--inner"
+          onEntered={showDispplay}>
+          <div
+            className={classNames('box--inner', {
+              'box--inner__align-middle': !weatherLoaded,
+            })}>
+            {weatherLoaded && displayMounted ? (
+              <Display city={city} daily={daily} currently={currently} />
+            ) : (
+              <Loading />
+            )}
+          </div>
+        </CSSTransition>
       </div>
     </main>
   )
--- client/src/components/Display/index.jsx
+++ client/src/components/Display/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 import Currently from '../Currently'
 import Daily from '../Daily'
