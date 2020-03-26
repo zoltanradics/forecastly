64567723051ea6577bd76ff47902bb953a42c973
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-26 15:29:03 +0100

    Introduce spinner to location autosuggest

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+import Loading from '../Loading'
 
 import {
   requestLocationAction,
   requestLocationSuggestionAction,
   requestWeatherDataAction,
+  dumpSuggestionAction,
 } from '../../redux/actions'
 
 const Search = () => {
   let timeout = null
   const dispatch = useDispatch()
   const inputRef = useRef()
+  const [isLoading, setLoading] = useState(false)
   const { suggestions } = useSelector((store) => store.location)
 
   const loadSuggestions = () => {
     if (inputRef.current.value.length >= 3) {
+      setLoading(true)
       dispatch(requestLocationSuggestionAction(inputRef.current.value))
+    } else {
+      setLoading(false)
+      dispatch(dumpSuggestionAction())
     }
   }
 
@@ -43,12 +51,19 @@ const Search = () => {
       </div>
       <form className="search__form">
         <div className="search__form__input-group">
-          <input
-            name="location"
-            placeholder="Enter your city"
-            onChange={handleOnCange}
-            ref={inputRef}
-          />
+          <div className="field">
+            <input
+              name="location"
+              placeholder="Enter your city"
+              onChange={handleOnCange}
+              ref={inputRef}
+            />
+            {isLoading && (
+              <div className="spinner">
+                <Loading />
+              </div>
+            )}
+          </div>
           <button onClick={showLocationSuggestions}>LOCATE ME!</button>
         </div>
         {suggestions && suggestions.length > 0 && (
