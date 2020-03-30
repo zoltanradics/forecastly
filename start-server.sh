a0dbc6b4c2b38ee61476620fc57e951248d90a21
Zoltan Radics <zoltan.radics@gmail.com>
2020-03-30 14:49:15 +0200

    Update comment

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,5 @@
 
-# Export requred env variables
+# Export requred env variables (Normally I would avoid storing keys in the GIT repository but for now it's OK.)
 export DARK_SKY_KEY=a62dec6100d36029ee4c6eb2c4b300e0 
 export OPEN_CAGE_KEY=bab4b0f60233456cbf247a0dc5131349 
 
