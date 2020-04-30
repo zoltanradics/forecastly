d0914cbdea38537b613002a2aa5db6a69cb13faf
Zoltan Radics <zoltan.radics@gmail.com>
2020-04-30 11:48:04 +0200

    Rename keys

--- server-node/index.js
+++ server-node/index.js
@@ -14,10 +14,11 @@ const PORT = 3000
 const isDev = process.env.NODE_ENV !== 'production'
 const testIpAddress = '77.57.123.202' // This IP address is for Zurich / Switzerland
 const IP_LOCATION_API_ENDPOINT = `https://ipapi.co/__IP__/json`
-const DARK_SKY_KEY = process.env.DARK_SKY_KEY || functions.config().key.dark_sky
-const DARK_SKY_API_ENDPOINT = `https://api.darksky.net/forecast/${DARK_SKY_KEY}`
-const OPEN_CAGE_KEY =
-  process.env.OPEN_CAGE_KEY || functions.config().key.open_cage
+const FORECASTLY_DARK_SKY_KEY =
+  process.env.FORECASTLY_DARK_SKY_KEY || functions.config().key.dark_sky
+const DARK_SKY_API_ENDPOINT = `https://api.darksky.net/forecast/${FORECASTLY_DARK_SKY_KEY}`
+const FORECASTLY_OPEN_CAGE_KEY =
+  process.env.FORECASTLY_OPEN_CAGE_KEY || functions.config().key.open_cage
 const OPEN_CAGE_API_ENDPOINT = `https://api.opencagedata.com/geocode/v1/json`
 
 // Init Firebase admin
@@ -30,10 +31,10 @@ app.use(cors())
 
 // Check if required API keys are defined
 app.use((req, res, next) => {
-  if (typeof DARK_SKY_KEY === 'undefined') {
-    res.status(500).json({ message: 'DARK_SKY_KEY is undefined!' })
-  } else if (typeof OPEN_CAGE_KEY === 'undefined') {
-    res.status(500).json({ message: 'OPEN_CAGE_KEY is undefined!' })
+  if (typeof FORECASTLY_DARK_SKY_KEY === 'undefined') {
+    res.status(500).json({ message: 'FORECASTLY_DARK_SKY_KEY is undefined!' })
+  } else if (typeof FORECASTLY_OPEN_CAGE_KEY === 'undefined') {
+    res.status(500).json({ message: 'FORECASTLY_OPEN_CAGE_KEY is undefined!' })
   } else {
     next()
   }
@@ -98,7 +99,7 @@ app.get(
 
     // Send HTTP request
     const response = await sendHttpRequest(OPEN_CAGE_API_ENDPOINT, {
-      key: OPEN_CAGE_KEY,
+      key: FORECASTLY_OPEN_CAGE_KEY,
       q: location,
       limit: 5,
     }).catch((_error) => {
